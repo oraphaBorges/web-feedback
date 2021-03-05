5e48b401be8fdf5e5dbfc5dc604a8a7c3588fbcc
Raphael Borges <rah-borges@hotmail.com>
2021-03-05 15:13:10 -0300

    style(home):add componets da home #265

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,14 +1,34 @@
 <template>
-<div>
-  <h1>Home</h1>
-</div>
+  <div>
+    <BannerHome />
+    <BoasVindas />
+    <Valores />
+    <Contador />
+    <ServicosHome />
+    <Testemunho />
+    <Newsletter />
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import BannerHome from './components/BannerHome.vue'
+import BoasVindas from './components/BoasVindas.vue'
+import Valores from './components/Valores.vue'
+import Contador from './components/Contador.vue'
+import ServicosHome from './components/ServicosHome.vue'
+import Testemunho from './components/Testemunho.vue'
+import Newsletter from './components/Newsletter.vue'
 
 @Component({
   components: {
+    BannerHome,
+    BoasVindas,
+    Valores,
+    Contador,
+    ServicosHome,
+    Testemunho,
+    Newsletter,
   },
 })
 export default class Home extends Vue {}
