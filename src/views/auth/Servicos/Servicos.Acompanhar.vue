fd1249f7c736a3e3fb71d0eb3198eae5fa67ccf8
Raphael Borges <rah-borges@hotmail.com>
2021-03-05 11:21:47 -0300

    style(acompanhar):add Banner #265

--- src/views/auth/Servicos/Servicos.Acompanhar.vue
+++ src/views/auth/Servicos/Servicos.Acompanhar.vue
@@ -1,7 +1,16 @@
 <template>
-<div>
-  <h1>Acompanhar serviço</h1>
-</div>
+    <section>
+        <div class="row d-flex align-items-center p-3">
+            <div class="col-sm-12 col-md-7 text-end">
+                <h2>Serviços</h2>
+                <p>Acompanhe aqui seus serviços solicitados</p>
+            </div>
+            <div class="col-sm-12 col-md-5">
+                <img class="img-service" :src="img" alt="Imagem ilustrativa de acompanhamento de serviços">
+            </div>
+        </div>
+        <hr>
+    </section>
 </template>
 
 <script lang="ts">
@@ -10,6 +19,17 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component({
   components: {
   },
+  data(){
+    return {
+        img:require('@/assets/img/undraw.co/acompanhar.png'),
+    }
+  },
 })
 export default class Acompanhar extends Vue {}
 </script>
+
+<style scoped>
+.img-service{
+    width: 400px;
+}
+</style>
