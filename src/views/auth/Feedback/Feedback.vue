81729489796d531946a9ce31c3f5c648b947f496
Raphael Borges <rah-borges@hotmail.com>
2021-03-05 11:17:03 -0300

    style(feedback):banner da página #265

--- src/views/auth/Feedback/Feedback.vue
+++ src/views/auth/Feedback/Feedback.vue
@@ -1,7 +1,17 @@
 <template>
-<div>
-  <h1>Ideias</h1>
-</div>
+  <section>
+    <div class="row d-flex align-items-center p-3">
+        <div class="col-sm-12 col-md-8 text-end ">
+            <h2>Tem algo para compartilhar? </h2>
+            <p>Caso você tenha alguma idéia, crítica ou sugestão para melhorar e educação na UFFS? Compartile conosco (e com toda a comunidade)</p>
+            <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#FeedbackModal">Escrever</button>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <img class="img-feedback" :src="img" alt="imagem ilustrativa de feedback">
+        </div>
+    </div>
+    <hr>
+  </section>
 </template>
 
 <script lang="ts">
@@ -10,6 +20,17 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component({
   components: {
   },
+  data(){
+    return {
+        img:require('@/assets/img/undraw.co/Collaboration.png'),
+    }
+  },
 })
 export default class Feedback extends Vue {}
 </script>
+
+<style scoped>
+.img-feedback{
+    width: 350px;
+}
+</style>
