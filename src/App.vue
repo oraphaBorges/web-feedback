09031d8adce19f3bcee766350e8a17dbba59e6ab
Raphael Borges <rah-borges@hotmail.com>
2021-03-05 10:43:07 -0300

    chore(Vue):init do vue #265

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <nav-menu />
+    <router-view/>
+  </div>
+</template>
+
+<script type="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import NavMenu from './components/shared/NavMenu';
+
+@Component({
+  components: {
+    'nav-menu':NavMenu,
+  },
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss">
+
+</style>
