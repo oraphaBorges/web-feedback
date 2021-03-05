7893e05265f2fd679592b999ab9016d7d8823c43
Raphael Borges <rah-borges@hotmail.com>
2021-03-05 11:26:42 -0300

    style(solicitar):add banner #265

--- src/views/auth/Servicos/Servicos.Solicitar.vue
+++ src/views/auth/Servicos/Servicos.Solicitar.vue
@@ -1,7 +1,17 @@
 <template>
-<div>
-  <h1>Solicitar serviço</h1>
-</div>
+    <section>
+        <div class="row d-flex align-items-center p-3">
+            <div class="col-sm-12 col-md-7 text-end">
+                <h2>Serviços</h2>
+                <p>Temos um conjunto muito variado de serviços que podem ser utilizados pela comunidade acadêmica. Precisa de algo? Nós podemos ajudar!</p>
+                <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#ServiceModal">Solicitar Serviço</button>
+            </div>
+            <div class="col-sm-12 col-md-5">
+                <img class="img-service" :src="img" alt="Imagem ilustratia de uma garota adicionando um serço">
+            </div>
+        </div>
+        <hr>
+    </section>
 </template>
 
 <script lang="ts">
@@ -10,6 +20,16 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component({
   components: {
   },
+  data(){
+    return {
+      img:require('@/assets/img/undraw.co/User_flow.png'),
+    }
+  },
 })
 export default class Solicitar extends Vue {}
 </script>
+<style scoped>
+.img-service{
+    width: 400px;
+}
+</style>
